36bd67b3f76d9c032060d76a8071a573773851c8
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-02-18 10:07:43 -0300

    Create a top toolbar for each terroir

--- src/components/Terroir.js
+++ src/components/Terroir.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui-icons/MoreVert';
+
 import './Terroir.css';
 
 const Terroir = ({ terroir }) => (
@@ -9,6 +12,9 @@ const Terroir = ({ terroir }) => (
       <h2 className='terroir__head__name' alt={terroir.description} title={terroir.description}>
         {terroir.name}
       </h2>
+      <IconButton aria-label='Options'>
+        <MoreVertIcon />
+      </IconButton>
     </div>
     <div className='terroir__toolbar'>
     </div>
