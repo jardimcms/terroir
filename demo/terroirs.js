4e2c371d09dd1e1928385710412195641d80e3a7
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-02-18 14:10:46 -0300

    Add the GridList to list all instantiated components

--- demo/terroirs.js
+++ demo/terroirs.js
@@ -3,7 +3,106 @@ export default [
     name: 'Área Lateral Esquerda',
     description: 'Área de destaques da esquerda.',
     plugins: [],
-    components: []
+    components: [
+      {
+        key: 1,
+        type: 'card-photo',
+        name: 'Card de fotos',
+        description: 'Componente de card de fotos.',
+        thumb: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+        data: {
+          photo: {
+            url: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+            caption: 'Minha foto',
+            rightsHolder: 'Someone'
+          }
+        }
+      },
+      {
+        key: 1,
+        type: 'card-photo',
+        name: 'Card de fotos',
+        description: 'Componente de card de fotos.',
+        thumb: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+        data: {
+          photo: {
+            url: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+            caption: 'Minha foto',
+            rightsHolder: 'Someone'
+          }
+        }
+      },
+      {
+        key: 1,
+        type: 'card-photo',
+        name: 'Card de fotos',
+        description: 'Componente de card de fotos.',
+        thumb: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+        data: {
+          photo: {
+            url: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+            caption: 'Minha foto',
+            rightsHolder: 'Someone'
+          }
+        }
+      },
+      {
+        key: 1,
+        type: 'card-photo',
+        name: 'Card de fotos',
+        description: 'Componente de card de fotos.',
+        thumb: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+        data: {
+          photo: {
+            url: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+            caption: 'Minha foto',
+            rightsHolder: 'Someone'
+          }
+        }
+      },
+      {
+        key: 1,
+        type: 'card-photo',
+        name: 'Card de fotos',
+        description: 'Componente de card de fotos.',
+        thumb: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+        data: {
+          photo: {
+            url: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+            caption: 'Minha foto',
+            rightsHolder: 'Someone'
+          }
+        }
+      },
+      {
+        key: 1,
+        type: 'card-photo',
+        name: 'Card de fotos',
+        description: 'Componente de card de fotos.',
+        thumb: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+        data: {
+          photo: {
+            url: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+            caption: 'Minha foto',
+            rightsHolder: 'Someone'
+          }
+        }
+      },
+      {
+        key: 1,
+        type: 'card-photo',
+        name: 'Card de fotos',
+        description: 'Componente de card de fotos.',
+        thumb: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+        data: {
+          photo: {
+            url: 'https://material-ui-next.com/static/images/grid-list/breakfast.jpg',
+            caption: 'Minha foto',
+            rightsHolder: 'Someone'
+          }
+        }
+      }
+    ]
   },
   {
     name: 'Área Central Topo',
--- src/components/Terroir.js
+++ src/components/Terroir.js
@@ -1,28 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { withStyles } from 'material-ui/styles';
+import withWidth, { isWidthUp } from 'material-ui/utils/withWidth';
+import compose from 'recompose/compose';
 import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui-icons/MoreVert';
+import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
 
 import './Terroir.css';
 
-const Terroir = ({ terroir }) => (
+const styles = () => ({
+  gridList: {
+    transform: 'translateZ(0)',
+  }
+});
+
+const Terroir = ({ terroir, classes, width }) => (
   <div className='terroir'>
     <div className='terroir__head'>
-      <h2 className='terroir__head__name' alt={terroir.description} title={terroir.description}>
+      <h2 className='terroir__head__name'
+        alt={terroir.description}
+        title={terroir.description}>
         {terroir.name}
       </h2>
       <IconButton aria-label='Options'>
         <MoreVertIcon />
       </IconButton>
     </div>
-    <div className='terroir__toolbar'>
+    <div className='terroir__body'>
+      <GridList
+        cols={isWidthUp('sm', width, false) ? 6 : 1}
+        cellHeight={250}
+        spacing={10}
+        className={classes.gridList}>
+        {terroir.components.map((component, i) => (
+          <GridListTile
+            key={`component-${component.key}-${i}`}>
+            <img src={component.thumb} alt={component.name} />
+            <GridListTileBar
+              title={component.name}
+              subtitle={component.description}
+            >
+            </GridListTileBar>
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   </div>
 );
 
 Terroir.propTypes = {
-  terroir: PropTypes.object
+  terroir: PropTypes.object,
+  classes: PropTypes.object.isRequired,
+  width: PropTypes.string.isRequired
 };
 
-export default Terroir;
+export default  compose(withStyles(styles), withWidth())(Terroir);
